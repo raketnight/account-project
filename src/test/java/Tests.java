d3d579e94cdd4b781563c8d4f518e40ace12cd5a
raketnight <raket.f.i.f.p@gmail.com>
2018-11-13 18:14:09 +0000

    json test commit

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -12,6 +12,13 @@ public class Tests {
     }
 
 
+    @Test
+    public void JsonTest(){
+        String json = service.jmapper().get("1").toString();
+
+        Assert.assertTrue(json.equals("{\"accoutnNumber\":\"1\",\"lastName\":\"Makhim\",\"firstName\":\"Raket\"}") || json.equals("{\"accoutnNumber\":\"1\",\"firstName\":\"Raket\",\"lastName\":\"Makhim\"}") || json.equals("{\"firstName\":\"Raket\",\"accoutnNumber\":\"1\",\"lastName\":\"Makhim\"}")|| json.equals("{\"firstName\":\"Raket\",\"lastName\":\"Makhim\",\"accoutnNumber\":\"1\"}")|| json.equals("{\"lastName\":\"Makhim\",\"accoutnNumber\":\"1\",\"firstName\":\"Raket\"}")|| json.equals("{\"lastName\":\"Makhim\",\"firstName\":\"Raket\",\"accoutnNumber\":\"1\"}"));
+    }
+
 
     @Test
     public void testAccount1(){
@@ -30,6 +37,8 @@ public class Tests {
 
         Assert.assertEquals(2, finder.FindInstanceOfFirstNameStreams("Romio"));
     }
+
+
 
 
 }
