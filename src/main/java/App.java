4366aa4f1283f7ec38015eafa5e29743a5fb6e3d
raketnight <raket.f.i.f.p@gmail.com>
2018-11-13 14:30:16 +0000

    Second commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,10 @@
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
 
 
-    public static void main(String []args){
+    public static void main(String []args) throws JsonProcessingException {
 
         Service service = new Service();
 
@@ -11,6 +12,10 @@ public class App {
         new Account("Aharan", "Manoharan", "2");
 
         System.out.print(service.getRightHereMap());
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(service.getRightHereMap());
+        System.out.print(json);
 
     }
 }
