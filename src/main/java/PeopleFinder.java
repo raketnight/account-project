0ca0efe33a68bb5fd67dd788a9a3cb90a339aace
raketnight <raket.f.i.f.p@gmail.com>
2018-11-13 16:43:18 +0000

    second commit

--- src/main/java/PeopleFinder.java
+++ src/main/java/PeopleFinder.java
@@ -3,6 +3,16 @@ public class PeopleFinder {
     public int FindInstanceOfFirstName(String name) {
         Service service = new Service();
 
-        return 0;
+        int count = 0;
+
+        for (String key : service.getRightHereMap().keySet()){
+
+            if(service.getRightHereMap().get(key).getFirstName().equals(name)) {
+                count++;
+            }
+        }
+
+        System.out.print(count);
+        return count;
     }
 }
